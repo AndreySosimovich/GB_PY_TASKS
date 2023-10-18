f21d42fe1c1699fdc0df1c7b847c2219f48cd511
«Andrey» <sandreyv@bk.ru>
2023-10-18 13:34:09 +0300

    Add Task_10

---
+++ Task_10.py
@@ -0,0 +1,16 @@
+coins = [1, 1, 1, 1, 0, 1]
+orel = 0
+reshka = 0
+for i in range(len(coins)):
+    if coins[i] == 1:
+        orel += 1
+    if coins[i] == 0:
+        reshka += 1
+if orel < reshka:
+    count = orel
+else:
+    count = reshka
+print(count)
+
+# coins = [1, 1, 1, 1, 0, 1]
+# print(coins.count(0) if coins.count(0) < coins.count(1) else coins.count(1))
